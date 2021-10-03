784e9d5f386a395fe0e12442790ba12a235e188d
unknown <git@github.com:Andrei199729/Prechu.git>
2021-10-03 22:46:10 +0300

    доработка проекта

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-let popupOpened = document.querySelector('.popup_opened');
-let btnEdit = document.querySelector('.btn_edit');
-let btnAdd = document.querySelector('.btn_add');
+const popup = document.querySelector('.popup');
+const popupOpenBtn = document.querySelector('.profile__btn_edit');
+const popupCloseBtn = document.querySelector('.popup__btn_close');
+const inputName = document.querySelector('.form__input_name');
+const inputDoer = document.querySelector('.form__input_doer');
+const form = document.querySelector('.form');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
-let btnClose = document.querySelector('.btn_close');
-let save = document.querySelector('.btn_save');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+// Model close
+function closePopup() {
+    popup.classList.remove('popup__opened');
+}
+popupCloseBtn.addEventListener('click', closePopup);
 
-btnEdit.addEventListener('click', function() {
-    popupOpened.style.display = "flex";
-});
+// Popup open
 
-btnClose.addEventListener('click', function() {
-    popupOpened.style.display = "none";
-});
+function openPopup() {
+    popup.classList.add('popup__opened');
+}
+popupOpenBtn.addEventListener('click', openPopup);
 
-save.addEventListener('click', function(e) {
-    e.preventDefault();
-    let inputName = document.querySelector('.form__input_name');
-    let inputDoer = document.querySelector('.form__input_doer');
+// Form submit
+
+function submitForm(event) {
+    event.preventDefault();
     profileTitle.textContent = inputName.value;
     profileSubtitle.textContent = inputDoer.value;
-    popupOpened.style.display = "none";
-});
+}
+form.addEventListener('submit', closePopup);
+form.addEventListener('submit', submitForm);
