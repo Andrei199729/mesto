9c7c8b295570350d72fc70c47013ccc6ec7b7ebc
unknown <git@github.com:Andrei199729/Prechu.git>
2021-10-05 12:10:09 +0300

    исправления новые

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 const popup = document.querySelector('.popup');
-const popupOpenBtn = document.querySelector('.profile__btn_edit');
-const popupCloseBtn = document.querySelector('.popup__btn_close');
-const inputName = document.querySelector('.form__input_name');
-const inputDoer = document.querySelector('.form__input_doer');
+const popupOpenBtn = document.querySelector('.profile__btn_form_edit');
+const popupCloseBtn = document.querySelector('.popup__close-btn');
 const form = document.querySelector('.form');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 // Model close
 function closePopup() {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 popupCloseBtn.addEventListener('click', closePopup);
 
 // Popup open
 
 function openPopup() {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
 }
 popupOpenBtn.addEventListener('click', openPopup);
 
@@ -24,8 +22,8 @@ popupOpenBtn.addEventListener('click', openPopup);
 
 function submitForm(event) {
     event.preventDefault();
-    profileTitle.textContent = inputName.value;
-    profileSubtitle.textContent = inputDoer.value;
+    profileTitle.textContent = document.formprofile.name.value;
+    profileSubtitle.textContent = document.formprofile.doer.value;
+    closePopup()
 }
-form.addEventListener('submit', closePopup);
 form.addEventListener('submit', submitForm);
