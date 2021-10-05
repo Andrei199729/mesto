de96ae1362090be4cc2ac75edcd0cd3e76c26c83
unknown <git@github.com:Andrei199729/Prechu.git>
2021-10-05 19:31:00 +0300

    исправление кнопки add и формы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const popup = document.querySelector('.popup');
 const popupOpenBtn = document.querySelector('.profile__edit-btn');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
+const nameInput = document.querySelector('.form__input_type_name');
+const jobInput = document.querySelector('.form__input_type_job');
 const form = document.querySelector('.form');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
@@ -15,6 +17,8 @@ popupCloseBtn.addEventListener('click', closePopup);
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 }
 popupOpenBtn.addEventListener('click', openPopup);
 
@@ -22,8 +26,8 @@ popupOpenBtn.addEventListener('click', openPopup);
 
 function submitForm(event) {
     event.preventDefault();
-    profileTitle.textContent = document.formprofile.name.value;
-    profileSubtitle.textContent = document.formprofile.doer.value;
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
     closePopup()
 }
 form.addEventListener('submit', submitForm);
