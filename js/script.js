04aa19809bad7fc090606f8d7bd7ead76a90a0f9
unknown <git@github.com:Andrei199729/Prechu.git>
2021-10-18 21:01:20 +0300

    ПР5

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const popupEditProfile = document.querySelector('.popup_edit-profile');
 const popupAddCard = document.querySelector('.popup_add-card');
+const popupViewImage = document.querySelector('.popup_view-image');
+const body = document.querySelector('.page');
 const popupEditProfileBtn = document.querySelector('.profile__edit-btn');
 const popupAddCardBtn = document.querySelector('.profile__add-card-btn');
 const popupCloseBtnEdit = document.querySelector('.popup__close-btn-edit');
 const popupCloseAddCard = document.querySelector('.popup__close-btn-add');
-// 
+const popupCloseViewImageBtn = document.querySelector('.view-image__close-btn');
 const nameInput = document.querySelector('.form__input_type_name');
 const jobInput = document.querySelector('.form__input_type_job');
 const formEdit = document.querySelector('.form__edit');
@@ -14,27 +16,41 @@ const linkInput = document.querySelector('.form__input_link_picture');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const elements = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template').content;
 
+window.addEventListener('load', () => {
+    document.querySelectorAll('.popup').forEach((popup) => popup.classList.add('popup_transition'));
+});
+
+// Закрытие popup
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
 }
 
+// Закрытие popup увеличенных изображений
+function closePopupViewImage() {
+    popupViewImage.classList.remove('popup_view-image_opened');
+}
+popupCloseViewImageBtn.addEventListener('click', closePopupViewImage);
+
+// Закрытие popup редактирования профиля
 function closePopupEditBtn() {
     closePopup(popupEditProfile);
 }
 popupCloseBtnEdit.addEventListener('click', closePopupEditBtn);
 
+// Закрытие popup добаавления карточек
 function closePopupAddCard() {
     closePopup(popupAddCard);
 }
 popupCloseAddCard.addEventListener('click', closePopupAddCard);
 
 
-// Popup open profile
+// Открытие popup
 function openPopup(popup) {
     popup.classList.add('popup_opened');
 }
-
+// Открытие popup редактирования профиля
 function openEditprofile() {
     openPopup(popupEditProfile);
     nameInput.value = profileTitle.textContent;
@@ -42,14 +58,23 @@ function openEditprofile() {
 }
 popupEditProfileBtn.addEventListener('click', openEditprofile);
 
-// Popup add card
+// Открытие popup добаавления карточек
 function openPopupAddCard() {
     openPopup(popupAddCard);
 }
 popupAddCardBtn.addEventListener('click', openPopupAddCard);
 
-// Form submit отправляет форму
+// Открытие popup увеличенных изображений
+function openViewImage(src, alt) {
+    popupViewImage.classList.add('popup_view-image_opened');
+    const viewImage = document.querySelector('.view-image__picture');
+    const viewCaption = document.querySelector('.view-image__caption');
+    viewImage.src = src;
+    viewImage.alt = alt;
+    viewCaption.textContent = alt;
+}
 
+// Form submit отправляет форму
 function editProfileSubmit(event) {
     event.preventDefault();
     profileTitle.textContent = nameInput.value;
@@ -91,8 +116,8 @@ const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-const cardTemplate = document.querySelector('#element-template').content;
 
+// Добавление карточек, удаление, лайк, открытие карточек
 function createCard(item) {
     const element = cardTemplate.querySelector('.element').cloneNode(true);
     element.querySelector('.element__image').src = item.link;
@@ -103,6 +128,13 @@ function createCard(item) {
     });
     element.querySelector('.element__btn').addEventListener('click', function(evt) {
         evt.target.classList.toggle('element__btn_like_active');
+    });
+    element.querySelector('.element__image').addEventListener('click', function(evt) {
+        evt.preventDefault();
+        const elementCard = evt.target.closest('.element');
+        const elementImageSrc = elementCard.querySelector('.element__image').src;
+        const elementmageAlt = elementCard.querySelector('.element__image').alt;
+        openViewImage(elementImageSrc, elementmageAlt);
     });
     return element;
 }
